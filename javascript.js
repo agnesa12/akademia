ca44b27288e49224683743248602ae1346758320
Your Name <agnesanuha4@gmail.com>
2020-08-09 23:24:00 +0200

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $(".wrapper").on("click", function() {
+      if($(".navbar .container>div").css("opacity") == "0") {
+        $(".navbar .container>div").css("opacity", "1");
+        $(".navbar2 .first").css("transform","translateY(13px) rotate(45deg)")
+        $(".navbar2 .second").css("opacity","0")
+        $(".navbar2 .third").css("transform","translateY(-13px) rotate(-45deg)")
+
+      }
+      else{
+        $(".navbar .container>div").css("opacity","0");
+        $(".navbar2 .first").css("transform","translateY(0) rotate(0)")
+        $(".navbar2 .second").css("opacity","1")
+        $(".navbar2 .third").css("transform","translateY(0) rotate(0)")
+      }
+    });
+});
+  $(document).ready(function(){
+    $('.slider2').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots:true,
+        arrows:false
+      });
+  })
